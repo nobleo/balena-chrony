6a6e769f8f752b2572dd61e6bf047bae9c9c3e78
Tim Clephas <tim.clephas@nobleo.nl>
2025-08-15 08:48:19 +0200

    Retry every minute just to be sure

--- systemd-stop-unit.bash
+++ systemd-stop-unit.bash
@@ -22,9 +22,18 @@ function wait_for_dbus() {
 }
 
 UNIT_NAME=$1
+SLEEP_DURATION=$2
 
 if [ -z "$UNIT_NAME" ]; then
-	echo "Usage: ./systemd-stop-unit.sh [unit name]"
+	echo "Usage: ./systemd-stop-unit.sh [unit name] [sleep_duration]"
+	echo "  sleep_duration: Number of seconds to sleep when unit becomes inactive (default: -1)"
+	echo "                  Use 0 or negative value to exit immediately"
+	exit 1
+fi
+
+# Set default behavior if no second argument provided
+if [ -z "$SLEEP_DURATION" ]; then
+	SLEEP_DURATION="-1"
 fi
 
 wait_for_dbus
@@ -37,6 +46,7 @@ unit_path=$(dbus-send --system \
 		      org.freedesktop.systemd1.Manager.GetUnit string:$UNIT_NAME \
 		      | awk '/object path/ {gsub("\"", "", $3); print $3}')
 
+last_active_state=""
 while true; do
 	active_state=$(dbus-send --system \
 				 --print-reply \
@@ -46,10 +56,17 @@ while true; do
 					string:org.freedesktop.systemd1.Unit \
 					string:ActiveState \
 				 | awk '/variant/ {gsub("\"","",$3); print $3}')
-
-	if [ "$active_state" = "inactive" ]; then
+	if [ "$last_active_state" != "$active_state" ]; then
 		echo "Unit $UNIT_NAME is now $active_state"
-		break;
+	fi
+	if [ "$active_state" = "inactive" ]; then
+		if [ "$SLEEP_DURATION" -le 0 ]; then
+			echo "Unit $UNIT_NAME is inactive, exiting immediately"
+			break
+		else
+			sleep "$SLEEP_DURATION"
+			break
+		fi
 	else
 		dbus-send --system \
 			  --type=method_call \
@@ -57,5 +74,8 @@ while true; do
 			  /org/freedesktop/systemd1 \
 			  org.freedesktop.systemd1.Manager.StopUnit \
 			  string:$UNIT_NAME string:replace
+		echo "Stopping unit $UNIT_NAME"
 	fi
+	last_active_state="$active_state"
+	sleep 1.0
 done
